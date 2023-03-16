507d9a209e6129cee621895d005c1b52d88fb28c
Megan Dacus <meganjustice0609@gmail.com>
2023-03-16 15:02:53 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+    return tutorials.map((sentence) => {
+    let wordsList = sentence.split(' ')
+    const titleCasedWords = wordsList.map(function(word){
+      return word.replace(word[0], word[0].toUpperCase());
+    })
+    return titleCasedWords.join(' ');
+});}
+
+
